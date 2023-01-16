ec99f5115474f7948053da60cc9acf8d60b1a778
samyakjha <210100131@iitb.ac.in>
2023-01-16 11:44:41 +0530

    g

--- alum.py
+++ alum.py
@@ -3,7 +3,7 @@ import numpy
 
 
 class BayesAlumEnv:
-    def __init__(self, variance_sq=None, priorArmMeans=numpy.array([]), priorArmVarSq=None, optimalArm=None):
+    def __init__(self, priorArmMeans, variance_sq=None,  priorArmVarSq=None, optimalArm=None):
         """
 
         :type priorArmMeans: ndarray
@@ -17,7 +17,7 @@ class BayesAlumEnv:
 
         if variance_sq is None:
             self.variance_sq = numpy.fill(len(priorArmMeans), 0.5)
-        if optimalArm is not None:
+        if optimalArm is None:
             self.optimalArm = numpy.where(numpy.isclose(priorArmMeans, max(priorArmMeans)))
         if priorArmVarSq is None:
             self.priorArmVarSq = numpy.fill(len(priorArmMeans), 0.5)
@@ -30,6 +30,10 @@ class BayesAlumEnv:
             if self.K[armIndex][0] > self.max_val:
                 self.max_val = self.K[armIndex]
 
+    def getOptimalArm(self):
+        return self.optimalArm
+
+
     def genDist(self):
         for i in range(len(self.priorArmMeans)):
             self.K = numpy.append(self.K,
@@ -38,7 +42,7 @@ class BayesAlumEnv:
         max_arm = numpy.where(numpy.isclose(self.K, self.max_val))
         spliced_array = numpy.delete(self.K, max_arm)
         numpy.random.shuffle(spliced_array)
-        spliced_arm_list_1, spliced_arm_list_2 = numpy.split(spliced_array, [max_arm, len(spliced_array) - max_arm])
+        spliced_arm_list_1, spliced_arm_list_2 = numpy.split(spliced_array, [self.optimalArm-1, len(spliced_array) - self.optimalArm])
         spliced_arm_list_1 = sorted(spliced_arm_list_1, key=lambda x: x[0])
         spliced_arm_list_2 = sorted(spliced_arm_list_2, key=lambda x: x[0])
         unimodality_mean_dist_inter = numpy.append(spliced_arm_list_1, self.max_val)
@@ -94,20 +98,61 @@ class BayesAlumEnv:
 
         return f_identified_arm
 
-    def bayesAlumAlgo(self, mean_dist, budget):
+    def bayesAlum(self, mean_dist, budget):
         arm_list = mean_dist
         arm_list_expand = numpy.array([])
         for index, arm in arm_list:
-            arm_list_expand = numpy.append(arm_list_expand, numpy.append(index, arm))
+            arm_list_expand[index] = numpy.array([index, arm, 0,0,0])
         index_list = numpy.arange(0, len(arm_list_expand))
 
         no_of_phases = numpy.floor(numpy.log(len(self.K / 3)) / numpy.log(3 / 2))
 
         for i in range(no_of_phases):
             phase = i + 1
-            arm_list_size = len(arm_list_expand)
-            b_list = numpy.array([arm_list_expand[0], arm_list_expand[numpy.ceil(arm_list_size / 3), arm_list_expand[numpy.floor(2*arm_list_size/3)], arm_list_expand[arm_list_size-1]]])
-            for pull in range(budget):
+            b_list = numpy.array([arm_list_expand[index_list[0]], arm_list_expand[index_list[numpy.ceil(len(index_list) / 3)]], arm_list_expand[index_list[numpy.floor(2 * len(index_list) / 3)]], arm_list_expand[index_list[len(index_list) - 1]]])
+            for marker_arms in b_list:
+                for pull in range(budget / 4):
+                    sample_val = numpy.random.normal(marker_arms[1][0], marker_arms[1][1])
+                    marker_arms[2] = marker_arms[2] + sample_val
+                    marker_arms[3] = marker_arms[3] + 1
+                marker_arms[4] = marker_arms[2] / marker_arms[3]
+
+            max_index = max(b_list, key=lambda x: x[4])
+
+            if max_index == 0 or max_index == 1:
+                index_list = index_list[0:numpy.floor(2 * len(index_list) / 3)]
+            else:
+                index_list = index_list[numpy.floor(len(index_list) / 3) - 1:len(index_list)]
+
+        phase = no_of_phases + 1
+        b_list = numpy.array([arm_list_expand[index_list[0]], arm_list_expand[index_list[numpy.ceil(len(index_list) / 3)]], arm_list_expand[index_list[len(index_list) - 1]]])
+        for marker_arms in b_list:
+            for pull in range(budget / 9):
+                sample_val = numpy.random.normal(marker_arms[1][0], marker_arms[1][1])
+                marker_arms[2] = marker_arms[2] + sample_val
+                marker_arms[3] = marker_arms[3] + 1
+            marker_arms[4] = marker_arms[2] / marker_arms[3]
+
+        phase = no_of_phases + 1
+        b_list = numpy.array([arm_list_expand[index_list[0]], arm_list_expand[index_list[numpy.ceil(len(index_list) / 3)]], arm_list_expand[index_list[len(index_list) - 1]]])
+        for marker_arms in b_list:
+            for pull in range(budget / 9):
+                sample_val = numpy.random.normal(marker_arms[1][0], marker_arms[1][1])
+                marker_arms[2] = marker_arms[2] + sample_val
+                marker_arms[3] = marker_arms[3] + 1
+            marker_arms[4] = marker_arms[2] / marker_arms[3]
+
+        b_identified_arm = b_list[numpy.argmax(b_list, key=lambda x: x[4])]
+        return b_identified_arm
+
+
+
+
+
+
+
+
+
 
 
 
--- experiment.py
+++ experiment.py
@@ -5,6 +5,7 @@ import StochasticBanditsModules2
 from bayesElim import BanditInstance, makeBandits, bandit_vars, bayesElim
 from bayesElim2 import bayesElim2
 from tqdm import tqdm
+import alum
 
 N_runs = 10000
 
@@ -19,6 +20,9 @@ maxArm = max(bandits_exp.meanlist)
 """
 num_mis_bayes = np.zeros(100)
 num_mis_bayes2 = np.zeros(100)
+num_mis_alum = np.zeros(100)
+num_mis_bayes_alum = np.zeros(100)
+
 #print(maxArm)
 
 for run in tqdm(range(N_runs)):
@@ -34,15 +38,24 @@ for run in tqdm(range(N_runs)):
             num_mis_bayes[j] += 1
         if abs(bayesElim2(bandits_exp, budget[j]).get_mean()-maxArm)>10e-4:
             num_mis_bayes2[j] += 1
-    
-    
-log_prob_mis_bayes = np.log((num_mis_bayes/N_runs))
-log_prob_mis_bayes2 = np.log((num_mis_bayes2/N_runs))
+
+for run in tqdm(range(N_runs)):
+    exp_env = alum.BayesAlumEnv(priori_means, var_list)
+    mean_dist = exp_env.genDist()
+    for j in range(100):
+        if (exp_env.bayesAlum(mean_dist, budget[j]) != exp_env.getOptimalArm()):
+            num_mis_bayes_alum[j] += 1
+        if (exp_env.alum(mean_dist, budget[j]) != exp_env.getOptimalArm()):
+            num_mis_alum[j] += 1
+
 #print(log_prob_mis)
 #print(num_mis)
 
-plt.plot(budget, log_prob_mis_bayes , color = 'red', label = 'bayesElim')
-plt.plot(budget, log_prob_mis_bayes2, color = 'blue', label = 'bayesElim2')
+plt.plot(budget, num_mis_bayes , color = 'red', label = 'bayesElim')
+plt.plot(budget, num_mis_bayes2, color = 'blue', label = 'bayesElim2')
+plt.plot(budget, num_mis_alum, color = 'green', label = 'alum')
+plt.plot(budget, num_mis_bayes_alum, color = 'orange', label = 'bayesAlum')
+
 plt.xlabel('budget values')
 plt.ylabel('log expected error probability')
 plt.yscale('log')
